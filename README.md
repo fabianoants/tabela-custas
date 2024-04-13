a6220583837719ac9f1827c1fc90ca78aad402a3
fabianoants <137217593+fabianoants@users.noreply.github.com>
2024-04-12 23:09:43 -0300

    atualicacoes

--- README.md
+++ README.md
@@ -4,12 +4,13 @@ Atualizações
 * 10/04 Layout do site;
 * 11/04 Geração aleatoria de valores;
 * 11/04 Comparação de valores;
+* 12/04 Gerar outro valor ao acertar;
+* 12/04 Contador de acertos;
 
 Em desenvolvimento
 * Otimizar codigo.
 
 Futuramente
-* Contador de acertos;
 * Numero maximo de tentativas para acertos;
 * Botão para reiniciar;
 * Barra de cronometro para acertar;
--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 let valorMaximo = 707200;
 let valorBase = gerarValorCustas();
 
-let valorBaseFormatado = formatarValor(valorBase);
-let valor = document.getElementById('valor');
-valor.innerHTML = `$${valorBaseFormatado}`;
+var valorAcertos = 0;
+console.log(valorAcertos);
 
 
 let valorCustas = document.querySelectorAll('.conteudo__lista__valores__botao');
@@ -14,6 +13,9 @@ console.log(valorCustas[0].value);
 
 function gerarValorCustas() {
     let valorGerado = parseInt(Math.random() * valorMaximo);
+    let valorBaseFormatado = formatarValor(valorGerado);
+    let valor = document.getElementById('valor');
+    valor.innerHTML = `$${valorBaseFormatado}`;
     return valorGerado;
 }
 
@@ -33,152 +35,179 @@ function formatarValor(atual) {
 
 function a() {
     if (valorBase <= 1403) {
-        valorCustas[0].classList.add('conteudo__lista__valores__botao-certo');
-        valorCustas[0].classList.remove('conteudo__lista__valores__botao'); 
+        valorAcertos++;
+        valorBase = gerarValorCustas();
+        limparBotoes();
     } else {
         valorCustas[0].classList.add('conteudo__lista__valores__botao-errado');
-        valorCustas[0].classList.remove('conteudo__lista__valores__botao'); 
+        valorCustas[0].classList.remove('conteudo__lista__valores__botao')
     }
 }
 
 function b() {
     if ((valorBase > 1403) && (valorBase <= 5302) ) {
-        valorCustas[1].classList.add('conteudo__lista__valores__botao-certo');
-        valorCustas[1].classList.remove('conteudo__lista__valores__botao'); 
+        valorAcertos++;
+        valorBase = gerarValorCustas();
+        limparBotoes();
     } else {
         valorCustas[1].classList.add('conteudo__lista__valores__botao-errado');
-        valorCustas[1].classList.remove('conteudo__lista__valores__botao'); 
+        valorCustas[1].classList.remove('conteudo__lista__valores__botao')
     }
 }
 
 function c() {
     if ((valorBase > 5302) && (valorBase <= 8838) ) {
-        valorCustas[2].classList.add('conteudo__lista__valores__botao-certo');
-        valorCustas[2].classList.remove('conteudo__lista__valores__botao'); 
+        valorAcertos++;
+        valorBase = gerarValorCustas();
+        limparBotoes();
     } else {
         valorCustas[2].classList.add('conteudo__lista__valores__botao-errado');
-        valorCustas[2].classList.remove('conteudo__lista__valores__botao'); 
+        valorCustas[2].classList.remove('conteudo__lista__valores__botao')
     }
 }
 
 function d() {
     if ((valorBase > 8838) && (valorBase <= 17680) ) {
-        valorCustas[3].classList.add('conteudo__lista__valores__botao-certo');
-        valorCustas[3].classList.remove('conteudo__lista__valores__botao'); 
+        valorAcertos++;
+        valorBase = gerarValorCustas();
+        limparBotoes();
     } else {
         valorCustas[3].classList.add('conteudo__lista__valores__botao-errado');
-        valorCustas[3].classList.remove('conteudo__lista__valores__botao'); 
+        valorCustas[3].classList.remove('conteudo__lista__valores__botao')
     }
 }
 
 function e() {
     if ((valorBase > 17680) && (valorBase <= 35360) ) {
-        valorCustas[4].classList.add('conteudo__lista__valores__botao-certo');
-        valorCustas[4].classList.remove('conteudo__lista__valores__botao'); 
+        valorAcertos++;
+        valorBase = gerarValorCustas();
+        limparBotoes();
     } else {
         valorCustas[4].classList.add('conteudo__lista__valores__botao-errado');
-        valorCustas[4].classList.remove('conteudo__lista__valores__botao'); 
+        valorCustas[4].classList.remove('conteudo__lista__valores__botao')
     }
 }
 
 function f() {
     if ((valorBase > 35360) && (valorBase <= 70720) ) {
-        valorCustas[5].classList.add('conteudo__lista__valores__botao-certo');
-        valorCustas[5].classList.remove('conteudo__lista__valores__botao'); 
+        valorAcertos++;
+        valorBase = gerarValorCustas();
+        limparBotoes();
     } else {
         valorCustas[5].classList.add('conteudo__lista__valores__botao-errado');
-        valorCustas[5].classList.remove('conteudo__lista__valores__botao'); 
+        valorCustas[5].classList.remove('conteudo__lista__valores__botao')
     }
 }
 
 function g() {
     if ((valorBase > 70720) && (valorBase <= 106080) ) {
-        valorCustas[6].classList.add('conteudo__lista__valores__botao-certo');
-        valorCustas[6].classList.remove('conteudo__lista__valores__botao'); 
+        valorAcertos++;
+        valorBase = gerarValorCustas();
+        limparBotoes();
     } else {
         valorCustas[6].classList.add('conteudo__lista__valores__botao-errado');
-        valorCustas[6].classList.remove('conteudo__lista__valores__botao'); 
+        valorCustas[6].classList.remove('conteudo__lista__valores__botao')
     }
 }
 
 function h() {
     if ((valorBase > 106080) && (valorBase <= 141440) ) {
-        valorCustas[7].classList.add('conteudo__lista__valores__botao-certo');
-        valorCustas[7].classList.remove('conteudo__lista__valores__botao'); 
+        valorAcertos++;
+        valorBase = gerarValorCustas();
+        limparBotoes();
     } else {
         valorCustas[7].classList.add('conteudo__lista__valores__botao-errado');
-        valorCustas[7].classList.remove('conteudo__lista__valores__botao'); 
+        valorCustas[7].classList.remove('conteudo__lista__valores__botao')
     }
 }
 
 function i() {
     if ((valorBase > 141440) && (valorBase <= 176800) ) {
-        valorCustas[8].classList.add('conteudo__lista__valores__botao-certo');
-        valorCustas[8].classList.remove('conteudo__lista__valores__botao'); 
+        valorAcertos++;
+        valorBase = gerarValorCustas();
+        limparBotoes();
     } else {
         valorCustas[8].classList.add('conteudo__lista__valores__botao-errado');
-        valorCustas[8].classList.remove('conteudo__lista__valores__botao'); 
+        valorCustas[8].classList.remove('conteudo__lista__valores__botao')
     }
 }
 
 function j() {
     if ((valorBase > 176800) && (valorBase <= 212160) ) {
-        valorCustas[9].classList.add('conteudo__lista__valores__botao-certo');
-        valorCustas[9].classList.remove('conteudo__lista__valores__botao'); 
+        valorAcertos++;
+        valorBase = gerarValorCustas();
+        limparBotoes();
     } else {
         valorCustas[9].classList.add('conteudo__lista__valores__botao-errado');
-        valorCustas[9].classList.remove('conteudo__lista__valores__botao'); 
+        valorCustas[9].classList.remove('conteudo__lista__valores__botao')
     }
 }
 
 function k() {
     if ((valorBase > 212160) && (valorBase <= 247520) ) {
-        valorCustas[10].classList.add('conteudo__lista__valores__botao-certo');
-        valorCustas[10].classList.remove('conteudo__lista__valores__botao'); 
+        valorAcertos++;
+        valorBase = gerarValorCustas();
+        limparBotoes();
     } else {
         valorCustas[10].classList.add('conteudo__lista__valores__botao-errado');
-        valorCustas[10].classList.remove('conteudo__lista__valores__botao'); 
+        valorCustas[10].classList.remove('conteudo__lista__valores__botao')
     }
 }
 
 function l() {
     if ((valorBase > 247520) && (valorBase <= 282880) ) {
-        valorCustas[11].classList.add('conteudo__lista__valores__botao-certo');
-        valorCustas[11].classList.remove('conteudo__lista__valores__botao'); 
+        valorAcertos++;
+        valorBase = gerarValorCustas();
+        limparBotoes();
     } else {
         valorCustas[11].classList.add('conteudo__lista__valores__botao-errado');
-        valorCustas[11].classList.remove('conteudo__lista__valores__botao'); 
+        valorCustas[11].classList.remove('conteudo__lista__valores__botao')
     }
 }
 
 function m() {
     if ((valorBase > 282880) && (valorBase <= 303851) ) {
-        valorCustas[12].classList.add('conteudo__lista__valores__botao-certo');
-        valorCustas[12].classList.remove('conteudo__lista__valores__botao'); 
+        valorAcertos++;
+        valorBase = gerarValorCustas();
+        limparBotoes();
     } else {
         valorCustas[12].classList.add('conteudo__lista__valores__botao-errado');
-        valorCustas[12].classList.remove('conteudo__lista__valores__botao'); 
+        valorCustas[12].classList.remove('conteudo__lista__valores__botao')
     }
 }
 
 function n() {
     if ((valorBase > 303851) && (valorBase <= 353600) ) {
-        valorCustas[13].classList.add('conteudo__lista__valores__botao-certo');
-        valorCustas[13].classList.remove('conteudo__lista__valores__botao'); 
+        valorAcertos++;
+        valorBase = gerarValorCustas();
+        limparBotoes();
     } else {
         valorCustas[13].classList.add('conteudo__lista__valores__botao-errado');
-        valorCustas[13].classList.remove('conteudo__lista__valores__botao'); 
+        valorCustas[13].classList.remove('conteudo__lista__valores__botao')
     }
 }
 
 function o() {
     if ((valorBase > 353600) && (valorBase <= 707200) ) {
-        valorCustas[14].classList.add('conteudo__lista__valores__botao-certo');
-        valorCustas[14].classList.remove('conteudo__lista__valores__botao'); 
+        valorAcertos++;
+        valorBase = gerarValorCustas();
+        limparBotoes();
     } else {
         valorCustas[14].classList.add('conteudo__lista__valores__botao-errado');
-        valorCustas[14].classList.remove('conteudo__lista__valores__botao'); 
+        valorCustas[14].classList.remove('conteudo__lista__valores__botao')
     }
 }
 
 /////////////////////////////////////////////
+
+function limparBotoes() {
+    for (let contagem = 0; contagem < valorCustas.length; contagem++) {
+        const contador = valorCustas[contagem];
+        valorCustas[contagem] = resetaClass(contador);
+    }
+}
+
+function resetaClass(resetar) {
+    resetar.classList.remove('conteudo__lista__valores__botao-errado');
+    resetar.classList.add('conteudo__lista__valores__botao');
+}
