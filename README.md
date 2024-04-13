7456b4f5d8289be79d977afe0fce5d3431693717
fabianoants <137217593+fabianoants@users.noreply.github.com>
2024-04-13 16:20:02 -0300

    encerrando

--- README.md
+++ README.md
@@ -6,13 +6,12 @@ Atualizações
 * 11/04 Comparação de valores;
 * 12/04 Gerar outro valor ao acertar;
 * 13/04 Contador de acertos;
+* Numero maximo de tentativas para acertos;
 
 Em desenvolvimento
-* Otimizar codigo.
+* Botão para reiniciar;
 
 Futuramente
-* Numero maximo de tentativas para acertos;
-* Botão para reiniciar;
 * Barra de cronometro para acertar;
 * Login com nome;
 * Rank de pontuação;
--- script.js
+++ script.js
@@ -9,6 +9,43 @@ function pontuacao(){
 }
 /////////////////////////////////////////////
 
+// Tentativas ///////////////////////////////
+let carimbosSemEfeito = document.querySelectorAll('.conteudo__tentativas__carimbos');
+console.log(carimbosSemEfeito);
+let valorErros = 0;
+function tentativas() {
+    valorErros++;
+    if (valorErros === 1) {
+        carimbosSemEfeito[0].classList.add('conteudo__tentativas__com-carimbo');
+        carimbosSemEfeito[0].classList.remove('conteudo__tentativas__sem-carimbo');
+    } 
+    if (valorErros === 2) {
+        carimbosSemEfeito[1].classList.add('conteudo__tentativas__com-carimbo');
+        carimbosSemEfeito[1].classList.remove('conteudo__tentativas__sem-carimbo');
+    }
+    if (valorErros === 3) {
+        carimbosSemEfeito[2].classList.add('conteudo__tentativas__com-carimbo');
+        carimbosSemEfeito[2].classList.remove('conteudo__tentativas__sem-carimbo');
+    }
+}
+/////////////////////////////////////////////
+
+// Reiniciar ////////////////////////////////
+/////////////////////////////////////////////
+
+// Encerrar /////////////////////////////////
+function cancelarBotoes() {
+    for (let cancelado = 0; cancelado < valorCustas.length; cancelado++) {
+        const zerado = valorCustas[cancelado];
+        valorCustas[cancelado] = desativar(zerado);
+    }
+}
+function desativar(inativo) {
+    inativo.classList.add('conteudo__lista__valores__botao-errado');
+    inativo.classList.remove('conteudo__lista__valores__botao');
+}
+/////////////////////////////////////////////
+
 // Função para gerar valor aleatorio ////////
 let valorBase = gerarValorCustas();
 function gerarValorCustas() {
@@ -51,10 +88,14 @@ function acertos() {
 function erros(errado) {
     errado.classList.add('conteudo__lista__valores__botao-errado');
     errado.classList.remove('conteudo__lista__valores__botao');
+    tentativas();
+    if (valorErros === 3) {
+        cancelarBotoes();
+    }
 }
 
 function a() {
-    if (valorBase <= 1403) {
+    if ((valorBase <= 1403) && (valorErros < 3)) {
         acertos();
     } else {
         erros(valorCustas[0]);
@@ -62,7 +103,7 @@ function a() {
 }
 
 function b() {
-    if ((valorBase > 1403) && (valorBase <= 5302) ) {
+    if ((valorBase > 1403) && (valorBase <= 5302) && (valorErros < 3)) {
         acertos();
     } else {
         erros(valorCustas[1]);
@@ -70,7 +111,7 @@ function b() {
 }
 
 function c() {
-    if ((valorBase > 5302) && (valorBase <= 8838) ) {
+    if ((valorBase > 5302) && (valorBase <= 8838) && (valorErros < 3)) {
         acertos();
     } else {
         erros(valorCustas[2]);
@@ -78,7 +119,7 @@ function c() {
 }
 
 function d() {
-    if ((valorBase > 8838) && (valorBase <= 17680) ) {
+    if ((valorBase > 8838) && (valorBase <= 17680) && (valorErros < 3)) {
         acertos();
     } else {
         erros(valorCustas[3]);
@@ -86,7 +127,7 @@ function d() {
 }
 
 function e() {
-    if ((valorBase > 17680) && (valorBase <= 35360) ) {
+    if ((valorBase > 17680) && (valorBase <= 35360) && (valorErros < 3)) {
         acertos();
     } else {
         erros(valorCustas[4]);
@@ -94,7 +135,7 @@ function e() {
 }
 
 function f() {
-    if ((valorBase > 35360) && (valorBase <= 70720) ) {
+    if ((valorBase > 35360) && (valorBase <= 70720) && (valorErros < 3)) {
         acertos();
     } else {
         erros(valorCustas[5]);
@@ -102,7 +143,7 @@ function f() {
 }
 
 function g() {
-    if ((valorBase > 70720) && (valorBase <= 106080) ) {
+    if ((valorBase > 70720) && (valorBase <= 106080) && (valorErros < 3)) {
         acertos();
     } else {
         erros(valorCustas[6]);
@@ -110,7 +151,7 @@ function g() {
 }
 
 function h() {
-    if ((valorBase > 106080) && (valorBase <= 141440) ) {
+    if ((valorBase > 106080) && (valorBase <= 141440) && (valorErros < 3)) {
         acertos();
     } else {
         erros(valorCustas[7]);
@@ -118,7 +159,7 @@ function h() {
 }
 
 function i() {
-    if ((valorBase > 141440) && (valorBase <= 176800) ) {
+    if ((valorBase > 141440) && (valorBase <= 176800) && (valorErros < 3)) {
         acertos();
     } else {
         erros(valorCustas[8]);
@@ -126,7 +167,7 @@ function i() {
 }
 
 function j() {
-    if ((valorBase > 176800) && (valorBase <= 212160) ) {
+    if ((valorBase > 176800) && (valorBase <= 212160) && (valorErros < 3)) {
         acertos();
     } else {
         erros(valorCustas[9]);
@@ -134,7 +175,7 @@ function j() {
 }
 
 function k() {
-    if ((valorBase > 212160) && (valorBase <= 247520) ) {
+    if ((valorBase > 212160) && (valorBase <= 247520) && (valorErros < 3)) {
         acertos();
     } else {
         erros(valorCustas[10]);
@@ -142,7 +183,7 @@ function k() {
 }
 
 function l() {
-    if ((valorBase > 247520) && (valorBase <= 282880) ) {
+    if ((valorBase > 247520) && (valorBase <= 282880) && (valorErros < 3)) {
         acertos();
     } else {
         erros(valorCustas[11]);
@@ -150,7 +191,7 @@ function l() {
 }
 
 function m() {
-    if ((valorBase > 282880) && (valorBase <= 303851) ) {
+    if ((valorBase > 282880) && (valorBase <= 303851) && (valorErros < 3)) {
         acertos();
     } else {
         erros(valorCustas[12]);
@@ -158,7 +199,7 @@ function m() {
 }
 
 function n() {
-    if ((valorBase > 303851) && (valorBase <= 353600) ) {
+    if ((valorBase > 303851) && (valorBase <= 353600) && (valorErros < 3)) {
         acertos();
     } else {
         erros(valorCustas[13]);
@@ -166,7 +207,7 @@ function n() {
 }
 
 function o() {
-    if ((valorBase > 353600) && (valorBase <= 707200) ) {
+    if ((valorBase > 353600) && (valorBase <= 707200) && (valorErros < 3)) {
         acertos();
     } else {
         erros(valorCustas[14]);
